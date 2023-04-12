372979690fedbc6f6a36a80be71e4d12f9a057f0
AWahba <ahmedheshamwahba@hotmail.com>
2023-04-12 15:55:04 +0200

    Setup up firebase

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -8,8 +8,16 @@ import './widgets/food_item_page.dart';
 import './widgets/signup_page.dart';
 import './widgets/login_page.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final cameras = await availableCameras();
 
   // Get a specific camera from the list of available cameras.
