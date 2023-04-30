56021aff167adbfc7e843c1adac81175e653bb7a
AWahba <ahmedheshamwahba@hotmail.com>
2023-04-30 16:21:40 +0300

    Added an intial loading screen when intializing app

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,59 +1,73 @@
 import 'package:camera/camera.dart';
-import 'package:client/models/history_model.dart';
+import 'package:client/models/user_history_model.dart';
 import 'package:client/widgets/predicting_food/camera_page.dart';
+import 'package:client/widgets/loading_screen/initial_loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'widgets/journaling/journal_page.dart';
-import 'widgets/food_item_details/food_item_page.dart';
 import 'widgets/authentication//signup_page.dart';
 import 'widgets/authentication/login_page.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await SystemChrome.setPreferredOrientations(
-    [
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ],
-  );
-  final cameras = await availableCameras();
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  await dotenv.load();
-
-  runApp(App(firstCamera));
+void main() async {
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  final camera;
-
-  App(this.camera);
+  late final camera;
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_)=> UserHistoryModel(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute:JournalPage.route,
-        routes: {
-          SignUpPage.route:(ctx)=> SignUpPage(),
-          LoginPage.route:(ctx)=> LoginPage(),
-          JournalPage.route:(ctx)=> JournalPage(),
-          CameraPage.route: (ctx) => CameraPage(camera: camera),
-        },
-      ),
+    return FutureBuilder(
+      future: initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ChangeNotifierProvider(
+            create: (_) => UserHistoryModel(),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              initialRoute: JournalPage.route,
+              routes: {
+                SignUpPage.route: (ctx) => SignUpPage(),
+                LoginPage.route: (ctx) => LoginPage(),
+                JournalPage.route: (ctx) => JournalPage(),
+                CameraPage.route: (ctx) => CameraPage(camera: camera),
+              },
+            ),
+          );
+        }
+        return MaterialApp(
+          home: LoadingScreen(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
+  }
+
+  Future<void> initializeApp() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Initialize firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // Allow only portrait mode
+    await SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ],
+    );
+
+    // Get a specific camera from the list of available cameras.
+    final cameras = await availableCameras();
+    camera = cameras.first;
+
+    // Load dotenv variables
+    await dotenv.load();
   }
 }
